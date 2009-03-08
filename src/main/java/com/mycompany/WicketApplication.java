a60b1acaaf19134c834b3310212249eaa9ef2f56
mix3 <himachocost333@hotmail.co.jp>
2009-03-08 22:55:42 +0900

    DBBoardPage fix

--- src/main/java/com/mycompany/WicketApplication.java
+++ src/main/java/com/mycompany/WicketApplication.java
@@ -3,6 +3,7 @@ package com.mycompany;
 import org.apache.wicket.guice.GuiceComponentInjector;
 import org.apache.wicket.protocol.http.WebApplication;
 
+import com.mycompany.page.DBBoardPage;
 import com.mycompany.page.LinkPage;
 import com.mycompany.page.TrainingPage;
 import com.mycompany.page.UploadPage;
@@ -16,8 +17,12 @@ public class WicketApplication extends WebApplication{
 	
     @Override
     protected void init() {
+    	getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
+    	getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
+    	
     	addComponentInstantiationListener(new GuiceComponentInjector(this));
     	
+    	mountBookmarkablePage("/DBBoardPage", DBBoardPage.class);
         mountBookmarkablePage("/TrainingPage", TrainingPage.class);
         mountBookmarkablePage("/LinkPage", LinkPage.class);
     }
--- src/main/java/com/mycompany/service/ServiceImpl.java
+++ src/main/java/com/mycompany/service/ServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mycompany.service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +13,7 @@ import net.java.ao.EntityManager;
 import net.java.ao.Query;
 
 import com.google.inject.Singleton;
+import com.mycompany.WicketApplication;
 import com.mycompany.entity.Article;
 import com.mycompany.entity.Comment;
 import com.mycompany.model.ArticleModel;
@@ -20,32 +24,32 @@ public class ServiceImpl implements Service{
 	private EntityManager em;
 	
 	public ServiceImpl() throws SQLException{
-//		Properties dbProperties = getDBProperties();
-//		String uri = dbProperties.getProperty("db.uri");
-//		String username = dbProperties.getProperty("db.username");
-//		String password = dbProperties.getProperty("db.password");
+		Properties dbProperties = getDBProperties();
+		String uri = dbProperties.getProperty("db.uri");
+		String username = dbProperties.getProperty("db.username");
+		String password = dbProperties.getProperty("db.password");
 		
-//		em = new EntityManager(new H2DatabaseProvider(uri, username, password));
-		em = new EntityManager(new H2DatabaseProvider("jdbc:h2:blog_db", "sa", ""));
+		em = new EntityManager(new H2DatabaseProvider(uri, username, password));
+//		em = new EntityManager(new H2DatabaseProvider("jdbc:h2:blog_db", "sa", ""));
 		Logger.getLogger("net.java.ao").setLevel(Level.FINE);
 		
 		em.migrate(Article.class, Comment.class);
 	}
 	
-//	private Properties getDBProperties() {
-//		Properties back = new Properties();
-//		InputStream is = WicketApplication.class.getResourceAsStream("/db.properties");
-//		if (is == null) {
-//			throw new RuntimeException("Unable to locate db.properties");
-//		}
-//		try {
-//			back.load(is);
-//			is.close();
-//		} catch (IOException e) {
-//			throw new RuntimeException("Unable to load db.properties");
-//		}
-//		return back;
-//	}
+	private Properties getDBProperties() {
+		Properties back = new Properties();
+		InputStream is = WicketApplication.class.getResourceAsStream("/db.properties");
+		if (is == null) {
+			throw new RuntimeException("Unable to locate db.properties");
+		}
+		try {
+			back.load(is);
+			is.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to load db.properties");
+		}
+		return back;
+	}
 	
 	public void createArticle(ArticleModel am) throws SQLException {
 //		em.create(Article.class, new DBParam[]{
